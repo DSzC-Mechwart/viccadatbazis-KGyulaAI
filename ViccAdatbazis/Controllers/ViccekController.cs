5aa595206ec1b1bdca1da681511e59f0f9ef99b7
Kovács Gyula <kovacs.gyula@burg.lo.hu>
2024-10-09 11:25:18 +0200

    Félkész

---
+++ ViccAdatbazis/Controllers/ViccekController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ViccAdatbazis.Data;
+using ViccAdatbazis.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViccAdatbazis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViccekController : ControllerBase
+    {
+        private readonly ViccDbContext _context;
+
+        public ViccekController(ViccDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/viccek
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vicc>>> GetViccek(int page = 1)
+        {
+            int pageSize = 10;
+            return await _context.Viccek
+                .Where(v => v.Aktiv)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        // POST: api/viccek
+        [HttpPost]
+        public async Task<ActionResult<Vicc>> PostVicc(Vicc vicc)
+        {
+            _context.Viccek.Add(vicc);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetViccek), new { id = vicc.Id }, vicc);
+        }
+
+        // PUT: api/viccek/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVicc(int id, Vicc vicc)
+        {
+            if (id != vicc.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vicc).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ViccExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/viccek/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVicc(int id)
+        {
+            var vicc = await _context.Viccek.FindAsync(id);
+            if (vicc == null)
+            {
+                return NotFound();
+            }
+
+            vicc.Aktiv = false; // Archiválás
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // POST: api/viccek/{id}/ertekeles
+        [HttpPost("{id}/ertekeles")]
+        public async Task<IActionResult> Ertekeles(int id, [FromBody] bool tetszik)
+        {
+            var vicc = await _context.Viccek.FindAsync(id);
+            if (vicc == null)
+            {
+                return NotFound();
+            }
+
+            if (tetszik)
+            {
+                vicc.Tetszik++;
+            }
+            else
+            {
+                vicc.NemTetszik++;
+            }
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool ViccExists(int id)
+        {
+            return _context.Viccek.Any(e => e.Id == id);
+        }
+    }
+}
--- ViccAdatbazis/Pages/Index.cshtml.cs
+++ ViccAdatbazis/Pages/Index.cshtml.cs
@@ -1,20 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ViccAdatbazis.Data;
+using ViccAdatbazis.Models;
 
 namespace ViccAdatbazis.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ViccDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ViccDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public List<Vicc> Viccek { get; set; }
 
+        public async Task OnGetAsync()
+        {
+            Viccek = await _context.Viccek
+                .Where(v => v.Aktiv)
+                .ToListAsync();
         }
     }
 }
