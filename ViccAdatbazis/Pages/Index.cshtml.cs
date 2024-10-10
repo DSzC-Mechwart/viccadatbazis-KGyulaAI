bc3c7d959fc93804c434818f69a8864cf3dfc848
Kovács Gyula <kovacs.gyula@burg.lo.hu>
2024-10-10 11:43:24 +0200

    haladunk

--- ViccAdatbazis/Pages/Index.cshtml.cs
+++ ViccAdatbazis/Pages/Index.cshtml.cs
@@ -1,32 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using ViccAdatbazis.Data;
-using ViccAdatbazis.Models;
 
 namespace ViccAdatbazis.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly ViccDbContext _context;
-
-        public IndexModel(ILogger<IndexModel> logger, ViccDbContext context)
+        public void OnGet()
         {
-            _logger = logger;
-            _context = context;
-        }
-
-        public List<Vicc> Viccek { get; set; }
-
-        public async Task OnGetAsync()
-        {
-            Viccek = await _context.Viccek
-                .Where(v => v.Aktiv)
-                .ToListAsync();
         }
     }
 }
